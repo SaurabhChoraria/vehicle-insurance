527af1a0312da4562d4efde3b83bb9785bbbdd8b
root <root@upshift-templatebot-deployment-6dc6c7689-mbzxh>
2022-07-05 06:30:11 +0000

    Fix for remediation :NetFlixFeign-NewInstance-NetFlixFeign_New_Instance

--- src/main/java/com/auto/insurance/Application.java
+++ src/main/java/com/auto/insurance/Application.java
@@ -3,7 +3,7 @@ package com.auto.insurance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
